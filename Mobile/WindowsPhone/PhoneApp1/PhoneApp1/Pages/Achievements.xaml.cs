c69ffbd50b1c77ccb34429512c27a3f8239d2cc8
Andro <akrezic@hotmail.com>
2014-11-22 21:41:32 +0100

    Predložak za pie chart i rating

--- Mobile/WindowsPhone/PhoneApp1/PhoneApp1/Pages/Achievements.xaml.cs
+++ Mobile/WindowsPhone/PhoneApp1/PhoneApp1/Pages/Achievements.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using Microsoft.Phone.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace PhoneApp1
 {
@@ -24,8 +25,20 @@ namespace PhoneApp1
             
             if (accountInfo.Username!= "")
             {
-                
+                int rating = getRating();
+                BitmapImage bm = new BitmapImage(new Uri(@"/Assets/Icons/FavoritesStar.png", UriKind.RelativeOrAbsolute));
+                BitmapImage bm_star = new BitmapImage(new Uri(@"/Assets/Icons/Favorites.png", UriKind.RelativeOrAbsolute));
+                star1.Source = rating >= 1 ? bm : bm_star;
+                star2.Source = rating >= 2 ? bm : bm_star;
+                star3.Source = rating >= 3 ? bm : bm_star;
+                star4.Source = rating >= 4 ? bm : bm_star;
+                star5.Source = rating >= 5 ? bm : bm_star;
             }
+        }
+
+        private int getRating()
+        {
+            return 4;
         }
     }
 }
