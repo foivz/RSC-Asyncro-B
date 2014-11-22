3b30c7d2d310759723e895c0c540fcc5667891fd
Dragutin <aquila10@windowslive.com>
2014-11-22 21:51:59 +0100

    login

--- Mobile/WindowsPhone/PhoneApp1/PhoneApp1/JSON/userData.cs
+++ Mobile/WindowsPhone/PhoneApp1/PhoneApp1/JSON/userData.cs
@@ -8,7 +8,9 @@ namespace PhoneApp1
 {
     public class userData
     {
-        public string Name { get; set; }
-        public string Surname { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string email { get; set; }
+        public string username { get; set; }
     }
 }
