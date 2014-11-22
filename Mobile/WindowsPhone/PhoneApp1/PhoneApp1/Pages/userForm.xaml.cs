3028e143e45442205932dd2064c1363c9ffea9c3
Dragutin <aquila10@windowslive.com>
2014-11-22 22:17:41 +0100

    login, registration, nesto

--- Mobile/WindowsPhone/PhoneApp1/PhoneApp1/Pages/userForm.xaml.cs
+++ Mobile/WindowsPhone/PhoneApp1/PhoneApp1/Pages/userForm.xaml.cs
@@ -30,6 +30,7 @@ namespace PhoneApp1
         {
             if (lozinka.Password == ponovanUnos.Password)
             {
+                SetProgress(true);
                 HttpClient client = new HttpClient();
                 var values = new List<KeyValuePair<string, string>>();
                 values.Add(new KeyValuePair<string, string>("username", korIme.Text));
@@ -40,14 +41,35 @@ namespace PhoneApp1
                 var content = new FormUrlEncodedContent(values);
                 var response = await client.PostAsync("http://188.226.168.226/api/register.php", content);
                 var responseString = await response.Content.ReadAsStringAsync();
-                MessageBox.Show(responseString);
 
+                if (responseString == "1")
+                {
+                    MessageBox.Show("User created successfully");
+                    SetProgress(false);
+                    NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+                }
+                else
+                {
+                    MessageBox.Show("Some error ccurred! Please try again");
+                    SetProgress(false);
+                }
                 
             }
             else
             {
                 MessageBox.Show("Password do not mach!");
             }
+        }
+        public void SetProgress(bool value)
+        {
+            if (SystemTray.ProgressIndicator == null)
+            {
+                SystemTray.ProgressIndicator = new ProgressIndicator();
+                SystemTray.ProgressIndicator.Text = "registering";
+            }
+
+            SystemTray.ProgressIndicator.IsIndeterminate = value;
+            SystemTray.ProgressIndicator.IsVisible = value;
         }
     }
 }
